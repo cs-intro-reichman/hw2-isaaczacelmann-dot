91a8a7f4d00b63c2925f8a4e97135db72f65558e
Zac Elmann <isaaczac.elmann@post.runi.ac.il>
2025-11-05 20:14:44 +0200

    Solution - HW2

--- Cheers.java
+++ Cheers.java
@@ -8,17 +8,19 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
                 String word = args[0];
-		int times = Integer.parseInt(args[1]);
+                int times = Integer.parseInt(args[1]);
 
-		String specialLetters = "AEFHILMNORSX";
-		word = word.toUpperCase();
-		for (int i = 0; i < word.length(); i++) {
-			char ch = word.charAt(i);
-			System.out.println(specialLetters.indexOf(ch) == -1 ? String.format("Give me a  %s: %s !", ch, ch)
-					: String.format("Give me an %s: %s !", ch, ch));
-		}
-		for (int j = 0; j < times; j++) {
-			System.out.println(String.format("%s !!!", word));
-		}              
+                String specialLetters = "AEFHILMNORSX";
+                word = word.toUpperCase();
+                for (int i = 0; i < word.length(); i++) {
+                        char ch = word.charAt(i);
+                        System.out.println(
+                                        specialLetters.indexOf(ch) == -1 ? String.format("Give me a  %s: %s !", ch, ch)
+                                                        : String.format("Give me an %s: %s !", ch, ch));
+                }
+                System.out.println("What does that spell?");
+                for (int j = 0; j < times; j++) {
+                        System.out.println(String.format("%s !!!", word));
+                }
         }
 }
